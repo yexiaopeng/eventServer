82468edfc63bb6ff85ba6f8de00a59268ef90c79
yexiaopeng <yexiaopeng1992@gmail.com>
2016-06-07 23:00:31 +0800

    基本通讯完成

--- source/main.cc
+++ source/main.cc
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
+#include "process/SONEEventProcess.h"
+
 int main(){
     printf("## Start Listen \r\n");
 
+    soneEventServer_init(21688,500);
 
     return 0;
 }
--- source/process/SONEEventProcess.cc
+++ source/process/SONEEventProcess.cc
@@ -41,13 +41,52 @@ int soneEventServer_init(int listenPort, int listenBacklog){
     printf("The End.");
 }
 void soneEventServer_do_accept(evutil_socket_t listener, short event, void *arg){
+    struct event_base *base = (struct event_base *)arg;
+    evutil_socket_t fd;
+    struct sockaddr_in sin;
+    socklen_t slen = sizeof(sin);
+    fd = accept(listener, (struct sockaddr *)&sin, &slen);
+    if (fd < 0) {
+        perror("accept");
+        return;
+    }
+    if (fd > FD_SETSIZE) { //这个if是参考了那个ROT13的例子，貌似是官方的疏漏，从select-based例子里抄过来忘了改
+        perror("fd > FD_SETSIZE\n");
+        return;
+    }
 
+    printf("ACCEPT: fd = %u\n", fd);
+
+    struct bufferevent *bev = bufferevent_socket_new(base, fd, BEV_OPT_CLOSE_ON_FREE);
+    bufferevent_setcb(bev, soneEventServer_read_cb, NULL, soneEventServer_error_cb, arg);
+    bufferevent_enable(bev, EV_READ|EV_WRITE|EV_PERSIST);
 }
 void soneEventServer_read_cb(struct bufferevent *bev, void *arg){
+#define MAX_LINE    256
+    char line[MAX_LINE+1];
+    int n;
+    evutil_socket_t fd = bufferevent_getfd(bev);
 
+    while (n = bufferevent_read(bev, line, MAX_LINE), n > 0) {
+        line[n] = '\0';
+        printf("fd=%u, read line: %s\n", fd, line);
+
+        bufferevent_write(bev, line, n);
+    }
 }
 void soneEventServer_error_cb(struct bufferevent *bev, short event, void *arg){
-
+    evutil_socket_t fd = bufferevent_getfd(bev);
+    printf("fd = %u, ", fd);
+    if (event & BEV_EVENT_TIMEOUT) {
+        printf("Timed out\n"); //if bufferevent_set_timeouts() called
+    }
+    else if (event & BEV_EVENT_EOF) {
+        printf("connection closed\n");
+    }
+    else if (event & BEV_EVENT_ERROR) {
+        printf("some other error\n");
+    }
+    bufferevent_free(bev);
 }
 void soneEventServer_write_cb(struct bufferevent *bev, void *arg){
 
